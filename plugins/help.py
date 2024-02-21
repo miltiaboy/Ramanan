4181bddc3b0159cd65a2c3d422e3e8c2e7e3027f
Abidpknew <105457327+Abidpknew@users.noreply.github.com>
2024-02-22 00:31:11 +0530

    Update help.py
    Sul

--- plugins/help.py
+++ plugins/help.py
@@ -10,7 +10,7 @@ async def grp(client, message):
     buttons = [
         [
             InlineKeyboardButton("🔍 𝖯𝗋𝗈𝖲𝖾𝖺𝗋𝖼𝗁 𝖥𝖺𝗍𝗁𝖾𝗋 🔎", url='http://t.me/ProsearchFatherbot?start=help'),
-            InlineKeyboardButton("🔎Movie Search Group🥀", url='https://t.me/+t-HcJA8ged9kNjI1')
+            InlineKeyboardButton("🔎 Join Movie Search Group 🔍", url='https://t.me/+t-HcJA8ged9kNjI1')
         ]
     ]
     
