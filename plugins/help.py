a5e2f2ad8f6ad3ebb44477c4ef6688366da8c6b1
Abidpknew <105457327+Abidpknew@users.noreply.github.com>
2024-02-22 11:52:22 +0530

    Update help.py
    Hmm

--- plugins/help.py
+++ plugins/help.py
@@ -8,16 +8,13 @@ CMD = ["/", "."]
 @Client.on_message(filters.private)
 async def grp(client, message):
  buttons = [[InlineKeyboardButton("🔍 𝖯𝗋𝗈𝖲𝖾𝖺𝗋𝖼𝗁 𝖥𝖺𝗍𝗁𝖾𝗋 🔎", url='http://t.me/ProsearchFatherbot?start=help')],[InlineKeyboardButton("🔎Movie Search Group🥀", url='https://t.me/+t-HcJA8ged9kNjI1')]]
-
-reply_markup = InlineKeyboardMarkup(buttons)
-    
-    await message.reply_photo(
+ reply_markup = InlineKeyboardMarkup(buttons)
+ await message.reply_photo(
         photo="http://graph.org/file/36a684f0c26c766870f7c.jpg",
         caption="<b> ⚠️ Oops!\n\nYou Can't Use Me for Searching Files from Private, I can only Work in Groups Now.\n\nUse @ProSearchFatherBot for Searching Files easily from private chat.\n\n Team @ProSearchFather !</b>",
         reply_markup=reply_markup,
         parse_mode=enums.ParseMode.HTML,
-        reply_to_message_id=message.id
-    )
+        reply_to_message_id=message.id)
         
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
