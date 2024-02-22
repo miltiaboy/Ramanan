72df4017b3de35f54ec93d365b9220e1cfedb4f9
Abidpknew <105457327+Abidpknew@users.noreply.github.com>
2024-02-22 09:23:41 +0530

    Update help.py
    Last

--- plugins/help.py
+++ plugins/help.py
@@ -7,12 +7,9 @@ CMD = ["/", "."]
 
 @Client.on_message(filters.private)
 async def grp(client, message):
-        buttons = [
-        [
-            InlineKeyboardButton("𝖯𝗋𝗈𝖲𝖾𝖺𝗋𝖼𝗁 𝖥𝖺𝗍𝗁𝖾𝗋", url='http://t.me/ProsearchFatherbot?start=help'),
-            InlineKeyboardButton("Movie Search Group", url='https://t.me/+t-HcJA8ged9kNjI1')
-        ]
-    ]
+    buttons = [[
+            InlineKeyboardButton("🔍 𝖯𝗋𝗈𝖲𝖾𝖺𝗋𝖼𝗁 𝖥𝖺𝗍𝗁𝖾𝗋 🔎", url='http://t.me/ProsearchFatherbot?start=help'),
+        ]]
 
     reply_markup = InlineKeyboardMarkup(buttons)
     
