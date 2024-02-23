da2b0ec56d72c5c013673e97d67e2f5c3b3e2c2f
Abidpknew <105457327+Abidpknew@users.noreply.github.com>
2024-02-24 01:27:17 +0530

    Update help.py
    New

--- plugins/help.py
+++ plugins/help.py
@@ -5,15 +5,16 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 
 CMD = ["/", "."]
 @Client.on_message(filters.private)
+@Client.on_message(filters.private)
 async def grp(client, message):
-buttons = [[InlineKeyboardButton("🍭 𝗣𝗿𝗼𝗦𝗲𝗮𝗿𝗰𝗵𝗙𝗮𝘁𝗵𝗲𝗿 𝗕𝗼𝘁 🍭", url='http://t.me/ProsearchFatherbot?start=help')],[InlineKeyboardButton("🍳 𝗠𝗼𝘃𝗶𝗲𝘀 & 𝗦𝗲𝗿𝗶𝗲𝘀 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🍳", url='https://t.me/+t-HcJA8ged9kNjI1')],[InlineKeyboardButton("🎬 𝗠𝗼𝘃𝗶𝗲𝘀 & 𝗦𝗲𝗿𝗶𝗲𝘀 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🚀", url='http://t.me/ProsearchFatherbot?start=help')]]
+buttons = [[InlineKeyboardButton("🍭 𝗣𝗿𝗼𝗦𝗲𝗮𝗿𝗰𝗵𝗙𝗮𝘁𝗵𝗲𝗿 𝗕𝗼𝘁 🍭", url='http://t.me/ProsearchFatherbot?start=help')],[InlineKeyboardButton("🍳 𝗠𝗼𝘃𝗶𝗲𝘀 & 𝗦𝗲𝗿𝗶𝗲𝘀 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🍳", url='https://t.me/+t-HcJA8ged9kNjI1')]]
 reply_markup = InlineKeyboardMarkup(buttons)
 await message.reply_photo(
-        photo="http://graph.org/file/36a684f0c26c766870f7c.jpg",
-        caption="<b> ⚠️ Oops!\n\nYou Can't Use Me for Searching Files from Private, I can only Work in Groups Now.\n\nUse @ProSearchFatherBot for Searching Files easily from private chat.\n\n Team @ProSearchFather !</b>",
-        reply_markup=reply_markup,
-        parse_mode=enums.ParseMode.HTML,
-        reply_to_message_id=message.id)
+        photo="http://graph.org/file/36a684f0c26c766870f7c.jpg",
+        caption="<b> ⚠️ Oops!\n\nYou Can't Use Me for Searching Files from Private, I can only Work in Groups Now.\n\nUse @ProSearchFatherBot for Searching Files easily from private chat.\n\n Team @ProSearchFather !</b>",
+        reply_markup=reply_markup,
+        parse_mode=enums.ParseMode.HTML,
+        reply_to_message_id=message.id)
         
 @Client.on_message(filters.command("ping", CMD))
 async def ping(_, message):
