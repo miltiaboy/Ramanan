2a957798b9e2b2f5281013b8006c632e75e84c5a
JamesJr0 <jamesjr4690@gmail.com>
2024-05-16 17:26:18 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -9,7 +9,7 @@ async def grp(client, message):
 	buttons = [[InlineKeyboardButton("🍭 𝗣𝗿𝗼𝗦𝗲𝗮𝗿𝗰𝗵𝗙𝗮𝘁𝗵𝗲𝗿 𝗕𝗼𝘁 🍭 ", url='http://t.me/ProsearchFatherbot?start=help')],[InlineKeyboardButton("🍳 𝗠𝗼𝘃𝗶𝗲𝘀 & 𝗦𝗲𝗿𝗶𝗲𝘀 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🍳", url='https://t.me/+t-HcJA8ged9kNjI1')],[InlineKeyboardButton("🎬 𝗠𝗼𝘃𝗶𝗲𝘀 & 𝗦𝗲𝗿𝗶𝗲𝘀 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 🚀 ", url='https://t.me/+-a7Vk8PDrCtiYTA9')],[InlineKeyboardButton("➕ 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽𝘀 ➕", url='http://t.me/Bae_Suzzy_bot?startgroup=true')]]
 	reply_markup = InlineKeyboardMarkup(buttons)
 	await message.reply_photo(
-		photo="http://graph.org/file/36a684f0c26c766870f7c.jpg",
+		photo="https://telegra.ph/file/d9aeabbc5150e0eee9541.jpg",
 		caption = "<b> ⚠️ Oops!\n\nYou Can't Use Me for Searching Files from Private, I can only Work in Groups Now.\n\nUse @ProSearchFatherBot for Searching Files easily from private chat.\n\n Team @ProSearchFather !</b>",
 		reply_markup=reply_markup,
 		parse_mode=enums.ParseMode.HTML,
