04b8849f8124ad154cfabb3586671d4444389409
JamesJr0 <jamesjr4690@gmail.com>
2024-11-22 08:41:52 +0530

    Update pm_filter.py
    Jr

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -466,7 +466,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
         InlineKeyboardButton('ᴜᴘᴅᴀᴛᴇꜱ', url=f"{UPDATES_CHANNEL}"),
-        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/+t-HcJA8ged9kNjI1'),
+        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/+R7lZTfsZ4k1mYjU9'),
         InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='start')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
