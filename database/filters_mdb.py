f2e078f8df17b7ebf504151404331ec78265e78f
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-12-14 09:30:37 +0530

    Update filters_mdb.py

--- database/filters_mdb.py
+++ database/filters_mdb.py
@@ -1,11 +1,11 @@
 import pymongo
 from pyrogram import enums
-from info import DATABASE_URI1, DATABASE_NAME
+from info import DATABASE_URI, DATABASE_NAME
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(DATABASE_URI1)
+myclient = pymongo.MongoClient(DATABASE_URI)
 mydb = myclient[DATABASE_NAME]
 
 
