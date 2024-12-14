46a003ecf2648f80e2454f165c5f5586a1a972f7
Matrix TG <108185896+TGxMATRIX@users.noreply.github.com>
2024-12-14 10:08:18 +0530

    Update ia_filterdb.py

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -350,3 +350,29 @@ def unpack_new_file_id(new_file_id):
     )
     file_ref = encode_file_ref(decoded.file_reference)
     return file_id, file_ref
+
+def get_readable_time(seconds) -> str:
+    """
+    Return a human-readable time format
+    """
+
+    result = ""
+    (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
+
+    if days != 0:
+        result += f"{days}d"
+    (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
+
+    if hours != 0:
+        result += f"{hours}h"
+    (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
+
+    if minutes != 0:
+        result += f"{minutes}m"
+
+    seconds = int(seconds)
+    result += f"{seconds}s"
+    return result
