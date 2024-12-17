5af6beab1bf30edde36cd42b7ce6b75a2eaf8065
kctelegram7 <kctelegram7@gmail.com>
2024-12-17 11:01:18 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from info import CHANNELS
-from database.ia_filterdb import save_file2, save_file3, save_file4, save_file5, check_file
+from database.ia_filterdb import save_file1, save_file2, save_file3, save_file4, save_file5, check_file
 media_filter = filters.document | filters.video | filters.audio
 
 @Client.on_message(filters.chat(CHANNELS) & media_filter)
@@ -19,25 +19,31 @@ async def media(bot, message):
     if message.id % 4 == 0:
         tru = await check_file(media)
         if tru == "okda":
+            await save_file1(media)
+        else:
+            print("skipped duplicate file from saving to DB 1")
+    elif message.id % 4 == 1:
+        tru = await check_file(media)
+        if tru == "okda":
             await save_file2(media)
         else:
-            print("skipped duplicate file from saving to db 😌")
-    elif message.id % 4 == 1:
+            print("skipped duplicate file from saving to DB 2")
+    elif message.id % 4 == 2:
         tru = await check_file(media)
         if tru == "okda":
             await save_file3(media)
         else:
-            print("skipped duplicate file from saving to db 😌")
-    elif message.id % 4 == 2:
+            print("skipped duplicate file from saving to DB 3")
+    elif message.id % 4 == 3:
         tru = await check_file(media)
         if tru == "okda":
             await save_file4(media)
         else:
-            print("skipped duplicate file from saving to db 😌")
+            print("skipped duplicate file from saving to DB 4")
     else:
         tru = await check_file(media)
         if tru == "okda":
             await save_file5(media)
         else:
-            print("skipped duplicate file from saving to db 😌")
+            print("skipped duplicate file from saving to DB 5")
     
