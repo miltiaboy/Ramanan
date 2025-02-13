c1222921c76e5f07b9153d2e3ff7749b6a16cd65
TgDominicSir <162572081+TgDominicSir@users.noreply.github.com>
2025-02-13 20:07:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ auth_users = [int(user) if id_pattern.search(user) else user for user in environ
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-
+POST_CHANNEL = int(os.getenv("POST_CHANNEL", "-1001842826681"))  # Replace with your Channel ID
 # MongoDB information
 USERS_DB = environ.get('USERS_DB', "")
 FILES_DB1 = environ.get('FILES_DB1', "")
