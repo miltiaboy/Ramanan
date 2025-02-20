544a1d38c765afbcfb81f19d85c4317845db90c7
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-02-20 09:52:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,26 +21,26 @@ BOT_TOKEN = environ['BOT_TOKEN']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/7e56d907542396289fee4.jpg https://telegra.ph/file/9aa8dd372f4739fe02d85.jpg https://telegra.ph/file/adffc5ce502f5578e2806.jpg https://telegra.ph/file/6937b60bc2617597b92fd.jpg https://telegra.ph/file/09a7abaab340143f9c7e7.jpg https://telegra.ph/file/5a82c4a59bd04d415af1c.jpg https://telegra.ph/file/323986d3bd9c4c1b3cb26.jpg https://telegra.ph/file/b8a82dcb89fb296f92ca0.jpg https://telegra.ph/file/31adab039a85ed88e22b0.jpg https://telegra.ph/file/c0e0f4c3ed53ac8438f34.jpg https://telegra.ph/file/eede835fb3c37e07c9cee.jpg https://telegra.ph/file/e17d2d068f71a9867d554.jpg https://telegra.ph/file/8fb1ae7d995e8735a7c25.jpg https://telegra.ph/file/8fed19586b4aa019ec215.jpg https://telegra.ph/file/8e6c923abd6139083e1de.jpg https://telegra.ph/file/0049d801d29e83d68b001.jpg')).split()
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5079242179').split()]
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '0').split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
+auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '5079242179').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-USERS_DB = environ.get('USERS_DB', "")
+USERS_DB = environ.get('USERS_DB', "mongodb+srv://Tesbot:Tesbot@cluster0.c5fduff.mongodb.net/?retryWrites=true&w=majority")
 FILES_DB1 = environ.get('FILES_DB1', "")
 FILES_DB2 = environ.get('FILES_DB2', "")
 FILES_DB3 = environ.get('FILES_DB3', "")
-FILES_DB4 = environ.get('FILES_DB4', "")
-FILES_DB5 = environ.get('FILES_DB5', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', '')
+FILES_DB4 = environ.get('FILES_DB4', "mongodb+srv://jaggu:jaggu@cluster0.hbix5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+FILES_DB5 = environ.get('FILES_DB5', "mongodb+srv://sree:sree@cluster0.vxbpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "ponjikkaramup")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'ponisir_files')
 
 # FSUB
 auth_channel = environ.get('AUTH_CHANNEL')
