3c5b57dcb597a9f60474c0f851ef863acf627ff6
miltiaboy <114646328+miltiaboy@users.noreply.github.com>
2025-02-20 13:47:38 +0530

    Update gfilters_mdb.py

--- database/gfilters_mdb.py
+++ database/gfilters_mdb.py
@@ -1,11 +1,11 @@
 import pymongo
-from info import FILES_DB5, DATABASE_NAME
+from info import USERS_DB, DATABASE_NAME
 from pyrogram import enums
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
-myclient = pymongo.MongoClient(FILES_DB5)
+myclient = pymongo.MongoClient(USERS_DB)
 mydb = myclient[DATABASE_NAME]
 
 
